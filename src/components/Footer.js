a042f3e419f1539d9ffd0f084cfcb9f5923f6db2
ShwetaD01 <shwetadonode819@gmail.com>
2023-12-31 23:15:42 +0530

    fix: links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,13 +17,13 @@ function Footer() {
           <h3>Designed and Developed by Shweta</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Shw</h3>
+          <h3>Copyright © {year} Shw.</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/Shweta-819"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -31,7 +31,7 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
-            <li className="social-icons">
+            {/* <li className="social-icons">
               <a
                 href="https://twitter.com/Soumyajit4419"
                 style={{ color: "white" }}
@@ -40,10 +40,10 @@ function Footer() {
               >
                 <AiOutlineTwitter />
               </a>
-            </li>
+            </li> */}
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/shwetadonode"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/shweta"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
